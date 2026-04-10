a49c1b2b595bfdb0cba65f78c4fdf76d88cd6b8e
Alsu Ismayilzade <Alsu.Ismayilzade@kibrit.tech>
2026-04-11 00:38:35 +0400

    swagger config

--- src/main/java/abb/tech/ticket_service/controller/EventController.java
+++ src/main/java/abb/tech/ticket_service/controller/EventController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping("/events")
 @Tag(name = "Event Controller", description = "Endpoints for managing corporate and social events")
 public class EventController {
 
